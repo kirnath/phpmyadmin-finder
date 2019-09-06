5cb778a290677e2c6a4226fc3e02b88670ce15db
Kirnath <inr404@gmail.com>
2019-09-07 00:36:19 +0700

    Update find.py

--- find.py
+++ find.py
@@ -58,9 +58,10 @@ def main():
 	time.sleep(2)
 	print warna.GREEN + "[+] Processing ",site + warna.ENDC
 	listed = open('data.txt', 'r')
-	baca = listed.read().split('\n')
+	head   = {'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
+	baca   = listed.read().split('\n')
 	for i in baca:
-		process = requests.get('http://{}{}'.format(site,i), verify=False)
+		process = requests.get('http://{}{}'.format(site,i), headers=head, verify=False)
 		status = str(process.status_code)
 		if "200" in status:
 			print warna.GREEN + "[+] Yeay! I've Found it!\n[+] ",process.url + warna.ENDC
